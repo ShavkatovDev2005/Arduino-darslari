bdb10b47fc84fe4f291d271796f6a094d0f93050
ShavkatovDev2005 <devshavkatov@gmail.com>
2025-11-06 10:41:01 +0500

    Update arduino-4.cpp

--- arduino-4.cpp
+++ arduino-4.cpp
@@ -1,23 +1,124 @@
-// 4-dars: Potensiometr bilan LED yorqinligini boshqarish
-
-int potPin = A0;   // Potensiometr markaz chiqishi
-int ledPin = 9;    // LED ulangan PWM pin
-
+int led1 = 8;
+int led2 = 9;
+int led3 = 10;
+int led4 = 11;
+int kVaqt = 1000;
 void setup() {
-  pinMode(ledPin, OUTPUT);  // LED chiqish pin
-  Serial.begin(9600);       // Serial monitor uchun
+  pinMode(led1, OUTPUT);// put your setup code here, to run once:
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
 }
 
 void loop() {
-  int potValue = analogRead(potPin);  // 0 - 1023 oralig‘ida o‘qish
-  int ledValue = map(potValue, 0, 1023, 0, 255);  // 0-255 oralig‘iga o‘tkazish
-  
-  analogWrite(ledPin, ledValue);  // LED yorqinligini boshqarish
-  
-  Serial.print("Pot qiymat: ");
-  Serial.print(potValue);
-  Serial.print(" | LED PWM: ");
-  Serial.println(ledValue);
-  
-  delay(50); // biroz kutish
+  // put your main code here, to run repeatedly:
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+
+  delay(kVaqt);
+
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, HIGH);
+
+  delay(kVaqt);
+
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, LOW);
+
+  delay(kVaqt);
+
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+
+  delay(kVaqt);
+
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+
+  delay(kVaqt);
+
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, HIGH);
+
+  delay(kVaqt);
+
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, LOW);
+
+  delay(kVaqt);
+
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+
+  delay(kVaqt);
+
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+
+  delay(kVaqt);
+
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, HIGH);
+
+  delay(kVaqt);
+
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, LOW);
+
+  delay(kVaqt);
+
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+
+  delay(kVaqt);
+
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+
+  delay(kVaqt);
+
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, HIGH);
+
+  delay(kVaqt);
+
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, LOW);
+
+  delay(kVaqt);
+
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
 }
