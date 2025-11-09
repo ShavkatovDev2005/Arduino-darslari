c4c4fc1220079c081e1a1b9cd4e93acae2c4d40b
ShavkatovDev2005 <devshavkatov@gmail.com>
2025-11-09 13:05:34 +0500

    Update arduino-5.cpp

--- arduino-5.cpp
+++ arduino-5.cpp
@@ -1,21 +1,9 @@
-int potPin = A0;   // Potensiometr markaz chiqishi
-int ledPin = 9;    // LED ulangan PWM pin
-
-void setup() {
-  pinMode(ledPin, OUTPUT);  // LED chiqish pin
-  Serial.begin(9600);       // Serial monitor uchun
+int ledPin = 10;
+int yoruglik = 255;
+void setup(){
+  pinMode(ledPin, OUTPUT);
 }
 
-void loop() {
-  int potValue = analogRead(potPin);  // 0 - 1023 oralig‘ida o‘qish
-  int ledValue = map(potValue, 0, 1023, 0, 255);  // 0-255 oralig‘iga o‘tkazish
-  
-  analogWrite(ledPin, ledValue);  // LED yorqinligini boshqarish
-  
-  Serial.print("Pot qiymat: ");
-  Serial.print(potValue);
-  Serial.print(" | LED PWM: ");
-  Serial.println(ledValue);
-  
-  delay(50); // biroz kutish
+void loop(){
+  analogWrite(ledPin, yoruglik);
 }
